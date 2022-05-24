c99187f42b8b5365469bacfa52d33b8e32f80e5b
Shahzad Majeed <smajeed@draftkings.com>
2022-05-24 18:05:03 -0400

    Update Dependencies.swift

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -6,6 +6,26 @@ import ProjectDescriptionHelpers
 // Build Configuration without XCConfigs
 
 //let projectSettings: Settings = .settings(configurations: BuildConfiguration.projectBuildConfigurations)
+//
+let enabledConfiguration: ProjectDescription.Project.ProjectConfiguration = .configuration(
+    options: .options(automaticSchemesOptions: .enabled(codeCoverageEnabled: true),
+                      developmentRegion: "en",
+                      disableBundleAccessors: false,
+                      disableShowEnvironmentVarsInScriptPhases: false,
+                      disableSynthesizedResourceAccessors: false,
+                      textSettings: .textSettings(),
+                      xcodeProjectName: nil),
+    resourceSynthesizers: .default)
+//
+//let disabledConfiguration: ProjectDescription.Project.ProjectConfiguration = .configuration(
+//    options: .options(automaticSchemesOptions: .disabled,
+//                      developmentRegion: "en",
+//                      disableBundleAccessors: true,
+//                      disableShowEnvironmentVarsInScriptPhases: true,
+//                      disableSynthesizedResourceAccessors: true,
+//                      textSettings: .textSettings(),
+//                      xcodeProjectName: nil),
+//    resourceSynthesizers: [])
 
 let dependencies = Dependencies(
     carthage: [],
@@ -13,13 +33,22 @@ let dependencies = Dependencies(
         [
             .local(path: .relativeToRoot("Projects/SPMModules"))
         ],
+        // productTypes: ["Feature1": .dynamicLibrary],
         baseSettings: .settings(
             configurations: [
                 .debug(name: "Test"),
                 .debug(name: "ODI"),
                 .release(name: "Prod")
             ]
-        )
+        ),
+        projectConfigurations: [
+            //"*": enabledConfiguration
+            
+            "SPMModules": enabledConfiguration,
+//            "Kingfisher": disabledConfiguration,  // Default behavior. No need to specify
+//            "SnapKit": disabledConfiguration // Default behavior. No need to specify
+//             
+        ]
     ),
     platforms: [.iOS]
 )
